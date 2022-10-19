344d03cde086cfe856ac0cc2347420f3fa3da7ed
etg-git <etg9307@gmail.com>
2022-10-19 14:51:46 +0900

    webview 적용

--- RnApp/App.js
+++ RnApp/App.js
@@ -1,40 +1,182 @@
-// import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 // import { Alert } from 'react-native';
-// import messaging from '@react-native-firebase/messaging';
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import {WebView} from 'react-native-webview';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  Alert,
+  BackHandler
+} from 'react-native';
+import MyWebView from './components/MyWebView';
 
 const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const setFireBaseToken = async () => {
+    const token = await messaging().getToken(); //토큰 나오는지 확인 로그로
+    console.log('token', token);
+  };
+
+  useEffect(() => {
+    setFireBaseToken();
+  }, []);
+
+  const foregroundListener = useCallback(() => {
+    messaging().onMessage(async message => {
+      console.log(message)
+    })
+  }, [])
+      
+  useEffect(() => {
+    foregroundListener()  
+  }, [])
+
+  // return (
+  //   <SafeAreaView style={styles.container}>
+  //     <Text style={styles.appTitle}>Hello Todolist</Text>
+  //   </SafeAreaView>
+  // );
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.appTitle}>Hello Todolist</Text>
-    </SafeAreaView>
+    <> 
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <SafeAreaView style={styles.root}>
+        <MyWebView 
+          // handleClose={()=>{
+          //   Alert.alert('앱 종료', '앱을 종료하시겠습니까?', [
+          //     {
+          //       text: '아니오',
+          //       onPress: () => null,
+          //     },
+          //     {text: '예', onPress: () => BackHandler.exitApp()},
+          //   ]);
+          // }}
+          />
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3143e8',
+  root: {
+    flex: 1
   },
-  appTitle: {
-    color: '#fff',
-    fontSize: 36,
-    marginTop: 30,
-    marginBottom: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    backgroundColor: '#3143e8',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#ffff',
+  // },
+  // appTitle: {
+  //   color: '#fff',
+  //   fontSize: 36,
+  //   marginTop: 30,
+  //   marginBottom: 30,
+  //   fontWeight: '300',
+  //   textAlign: 'center',
+  //   backgroundColor: '#3143e8',
+  // },
+  // appContent: {
+  //   color: '#fff',
+  //   fontSize: 20,
+  //   marginTop: 50,
+  //   marginBottom: 50,
+  //   fontWeight: '300',
+  //   textAlign: 'center',
+  //   backgroundColor: '#3143e8',
+  // },
 });
 
-// function App() {
-//   useEffect(() => {
-//     const unsubscribe = messaging().onMessage(async remoteMessage => {
-//       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-//     });
-
-//     return unsubscribe;
-//   }, []);
-// }
 export default App;
+
+
+// import React, { useState, useEffect, useCallback } from 'react';
+// // import { Alert } from 'react-native';
+// import messaging from '@react-native-firebase/messaging';
+// import {Text, StyleSheet, View, Button} from 'react-native';
+// import MyWebView from './components/MyWebView';
+// import {localNotificationService} from './src/LocalNotificationService';
+
+// export default function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const setFireBaseToken = async () => {
+//     const token = await messaging().getToken(); //토큰 나오는지 확인 로그로
+//     console.log('token', token);
+//   };
+
+//   useEffect(() => {
+//     setFireBaseToken();
+//   }, []);
+
+//   const foregroundListener = useCallback(() => {
+//     messaging().onMessage(async message => {
+//       console.log('foreground', message)
+//     })
+//   }, [])
+      
+//   useEffect(() => {
+//     foregroundListener()  
+//   }, [])
+
+//   useEffect(() => {
+//     fcmService.registerAppWithFCM();
+//     fcmService.register(onRegister, onNotification, onOpenNotification);
+//     localNotificationService.configure(onOpenNotification);
+
+//     function onRegister(token) {
+//       console.log('[App] onRegister : token :', token);
+//     }
+
+//     function onNotification(notify) {
+//       console.log('[App] onNotification : notify :', notify);
+//       const options = {
+//         soundName: 'default',
+//         playSound: true,
+//       };
+//       localNotificationService.showNotification(
+//         0,
+//         notify.title,
+//         notify.body,
+//         notify,
+//         options,
+//       );
+//     }
+
+//     function onOpenNotification(notify) {
+//       console.log('[App] onOpenNotification : notify :', notify);
+//       alert('Open Notification : notify.body :' + notify.body);
+//     }
+//     return () => {
+//       console.log('[App] unRegister');
+//       fcmService.unRegister();
+//       localNotificationService.unregister();
+//     };
+//   }, []);
+//   return (
+//     <> 
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <SafeAreaView style={styles.root}>
+//         <MyWebView 
+//           // handleClose={()=>{
+//           //   Alert.alert('앱 종료', '앱을 종료하시겠습니까?', [
+//           //     {
+//           //       text: '아니오',
+//           //       onPress: () => null,
+//           //     },
+//           //     {text: '예', onPress: () => BackHandler.exitApp()},
+//           //   ]);
+//           // }}
+//           />
+//       </SafeAreaView>
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
---
+++ RnApp/components/MyWebView.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import {BackHandler} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const MyWebView= ({handleClose}) => {
+  const BASE_URL = 'https://mshms.andami.kr';
+  // const [webview, setWebview] = useState();
+  // const [goBackable, setGoBackable] = useState(false);
+  // useEffect(() => {
+  //   const backHandler = BackHandler.addEventListener(
+  //     'hardwareBackPress',
+  //     () => {
+  //       console.log('goBackable', goBackable);
+  //       if (goBackable) webview.goBack();
+  //       else handleClose();
+  //       return true;
+  //     },
+  //   );
+  //   return () => backHandler.remove();
+  // }, [goBackable]);
+
+  // useEffect(() => {
+  //   if (webview && webview.clearCache) webview.clearCache();
+  // }, [webview]);
+  return (
+    <WebView
+      pullToRefreshEnabled={true}
+      startInLoadingState={true}
+      allowsBackForwardNavigationGestures={true}
+      // HTML이나, uri를 적어주는 부분
+      source={{uri: BASE_URL}} 
+      mixedContentMode={'compatibility'}
+      originWhitelist={['https://*', 'http://*']}
+      overScrollMode={'never'}
+      // ref={(ref) => setWebview(ref)}
+
+      // View가 로드될 때 자바스크립트를 웹 페이지에 주입
+      injectedJavaScript={` 
+      (function() {
+          function wrap(fn) {
+          return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage(window.location.href);
+              return res;
+          }
+          }
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+          window.ReactNativeWebView.postMessage(window.location.href);
+          });
+      })();
+      true;
+      `}
+
+      // WebView가 window.postMessage를 call하면 호출되는 것
+      onMessage={(event) => {
+        const url = event.nativeEvent.data;
+        // setGoBackable(url !== BASE_URL);
+        console.log('onMessage', event.nativeEvent.data);
+      }}
+    />
+  );
+};
+
+export default MyWebView;
